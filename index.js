7d44b0898eda5903f22ab81d0c9389dc8f478eae
bcharlotin1 <biancacharlotin1@gmail.com>
2021-08-25 12:30:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const card =[]
+
+function writeCards(array, eventName){
+    for(let i = 0; i< array.length; i++){
+      card.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`)
+    }
+return card
+}
+
+function countDown(posititveInteger){
+    while(posititveInteger >= 0){
+        console.log(posititveInteger--);
+    }
+}
